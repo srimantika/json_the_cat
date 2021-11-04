4658819388e1c4c6fbf3e3be59c6878908c8fccf
srimantika <srimantika@gmail.com>
2021-11-04 18:04:39 +0000

    Refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -28,10 +28,7 @@ const fetchBreedDescription = function(breedName, callback) {
 
 };
 
-fetchBreedDescription(breedName, (error, desc) => {
-  if (error) {
-    console.log('Error fetch details:', error);
-  } else {
-    console.log(desc);
-  }
-});
+
+
+
+module.exports = { fetchBreedDescription };
